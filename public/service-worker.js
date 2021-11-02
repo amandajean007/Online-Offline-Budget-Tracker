24fa119c391816033da20c468ae2c0f8f9db043e
Amanda McBee <ahanes35@yahoo.com>
2021-11-01 21:28:04 -0600

    working on service-worker and linking it up

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,10 +3,13 @@ const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
-  "/favicon.ico",
-  "/manifest.webmanifest",
-  "/assets/css/style.css",
-  "/assets/js/loadImages.js"
+  "/css/styles.css",
+  "/js/index.js",
+  "/js/db.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
 ];
 
 // install
