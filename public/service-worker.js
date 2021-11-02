7c3858ef715e4b409723a22112e3ff9510cd5b67
Amanda McBee <ahanes35@yahoo.com>
2021-11-02 09:35:46 -0600

    trying to fix service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "static-cache-v2";
+const STATIC_CACHE = "static-cache-v2";
 const RUNTIME_CACHE = `runtime-cache`;
 const FILES_TO_CACHE = [
   "/",
@@ -6,73 +6,83 @@ const FILES_TO_CACHE = [
   "/css/styles.css",
   "/js/index.js",
   "/js/db.js",
-  "/manifest.webmanifest",
+  "/manifest.json",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
   "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
 ];
 
 // install
-self.addEventListener("install", function (evt) {
+self.addEventListener("install", function (event) {
   // pre cache all static assets
-  evt.waitUntil(
+  event.waitUntil(
     caches
-      .open(CACHE_NAME)
-      .then(cache => cache.addAll("FILES_TO_CACHE"))
+      .open(STATIC_CACHE)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
       .then(() => self.skipWaiting())
   );
 });
 
 // activate
-self.addEventListener("activate", function(evt) {
+self.addEventListener("activate", event => {
+  const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
   // remove old caches
-  evt.waitUntil(
+  event.waitUntil(
     caches
       .keys()
-      .then(cacheNames =>
-        cacheNames.filter(cacheName => !currentCaches.includes(cacheName))
-      )
-      .then(cachesToDelete =>
-        Promise.all(
-          cachesToDelete.map(cacheToDelete => caches.delete(cacheToDelete))
-        )
-      )
+      .then(cacheNames => {
+        return cacheNames.filter(
+          cacheName => !currentCaches.includes(cacheName)
+        );
+      })
+      .then(cachesToDelete => {
+        return Promise.all(
+          cachesToDelete.map(cacheToDelete => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
       .then(() => self.clients.claim())
   );
 });
 
 // fetch
-self.addEventListener("fetch", function(evt) {
+self.addEventListener("fetch", event => {
   // cache successful GET requests to the API
-  if (evt.request.url.includes("/api/transaction")) {
-    evt.respondWith(
-      caches.open(RUNTIME_CACHE).then(cache =>
-        fetch(evt.request)
-          .then(response => {
-            // If the response was good, clone it and store it in the cache.
-              cache.put(evt.request, response.clone());
-              return response;
+  if (
+    event.request.method !== "GET" ||
+    !event.request.url.startsWith(self.location.origin)
+    ) {
+      event.respondWith(
+        caches.open(RUNTIME_CACHE).then(cache => {
+          return fetch(event.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+                cache.put(event.request, response.clone());
+                return response;
+            })
+            .catch(() => caches.match(event.request))
           })
-          .catch(() => caches.match(evt.request))
-      )
-    );
+      );
   // stop execution of the fetch event callback
     return;
   }
 
   // if the request is not for the API, serve static assests using "offline-first" approach
-  evt.respondWith(
-    caches.match(evt.request).then(cachedResponse => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
       if (cachedResponse) {
         return cachedResponse;
       }
       return caches
         .open(RUNTIME_CACHE)
-        .then(cache =>
-          fetch(evt.request).then(response =>
-            cache.put(evt.request, response.clone()).then(() => response)
-          )
-        );
+        .then(cache => {
+          return fetch(event.request).then(response => {
+            return cache.put(event.request, response.clone()).then(() => {
+              return response
+            });
+          });
+        });
     })
   );
 });
